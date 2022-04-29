bcb6ae1278dfffaa7e16da6450cef7f80f3665ae
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2022-04-29 11:22:01 +0900

    [Modify] 회원가입 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,11 @@
-from rest_framework             import status
 from rest_framework.permissions import AllowAny
-from rest_framework.response    import Response
-from rest_framework.views       import APIView
+from rest_framework.generics import CreateAPIView
 
 from .serializers import UserSerializer
 
-class UserView(APIView):
+class SignUpView(CreateAPIView):
     permission_classes = [AllowAny]
-
+    serializer_class = UserSerializer
+    
     def post(self, request):
-        serializer = UserSerializer(data = request.data)
-        serializer.is_valid(raise_exception = True)
-        serializer.save() 
-
-        return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return self.create(request)
